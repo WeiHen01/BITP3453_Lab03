80942c8dc02f8bc1d32d93eae85de790286853c1
NG WEI HEN <rolandng0001@gmail.com>
2023-10-24 15:33:52 +0800

    BITP3453_Lab03

--- lib/dailyexpenses.dart
+++ lib/dailyexpenses.dart
@@ -40,26 +40,18 @@ class _ExpenseListState extends State<ExpenseList> {
  void _addExpense(){
    String description = descriptionController.text.trim();
    String amount = amountController.text.trim();
-   double cost = double.parse(amount);
-
 
    if(description.isNotEmpty && amount.isNotEmpty){
      setState(() {
-
+       total = double.parse(amount) + total;
        expenses.add(Expense(description, amount));
+       // display the output in text field
+       totalController.text = total.toStringAsFixed(1);
 
-       /**
-        * Iteration
-        */
-       for(Expense index in expenses)
-         {
-           total = total + double.parse(index.amount);
-         }
        descriptionController.clear();
        amountController.clear();
 
-       // display the output in text field
-       totalController.text = total.toStringAsFixed(1);
+
 
      });
    }
@@ -68,34 +60,14 @@ class _ExpenseListState extends State<ExpenseList> {
  void _removeExpense(int index){
    setState(() {
 
-     /**
-      *   double.tryParse(expenses[index].amount):
-      *   ------------------------------------------------------------------------
-      *   - This line attempts to parse the amount stored in the expenses list
-      *     at the specified index into a double.
-      *   - If the parsing is successful, parsedAmount will be assigned the parsed
-      *     double value.
-      *   - If the parsing fails (e.g., if the amount is not a valid number),
-      *     parsedAmount will be set to null.
-      *
-      *   parsedAmount!:
-      *   ----------------------------------------------------------------------
-      *   - The ! operator is used to assert that you are sure that parsedAmount
-      *     is not null.
-      *   - This is done because parsedAmount is defined as a nullable double,
-      *     and you want to use it as a non-nullable double in the subsequent operations.
-      *
-      *   total -= parsedAmount!;:
-      *   ----------------------------------------------------------------------
-      *   - If the parsing was successful, this line subtracts the parsed amount
-      *     (non-nullable double) from the total variable.
-      *
-      * */
-
-     double ? parsedAmount = double.tryParse(expenses[index].amount);
-     total -= parsedAmount!;
+     double cost = double.parse(expenses[index].amount);
+     total = total - cost;
      totalController.text = total.toString();
      expenses.removeAt(index);
+
+     if(expenses.length == 0){
+       totalController.text = '0.0';
+     }
 
    });
  }
